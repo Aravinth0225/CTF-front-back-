14123e6d36bf87331eeb4bf09d0af0dace0308ca
Aravind <aravinthjaya525@gmail.com>
2025-02-22 14:07:27 +0530

    Adding challenge components

---
+++ ctf/src/components/challenges/Challenge0x06/Challenge0x06.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Challenge6.css"; 
+import bgVideo from "../video/video bg ctf.mp4"; // Ensure you have the video file in the correct path
+
+const Challenge0x06 = () => {
+  return (
+    <div className="challenge-wrapper">
+      {/* Background Video */}
+      <video autoPlay loop muted className="background-video">
+        <source src={bgVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Challenge Content */}
+      <div className="challenge-content">
+        <h1 className="challenge-header">Challenge 0x06</h1>
+        <p className="challenge-instruction">Challenge Description</p>
+      </div>
+    </div>
+  );
+};
+
+export default Challenge0x06;
